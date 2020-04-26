2d5674cf4f51f5b8c0f484ddfb593ba60d88140c
bopboy2 <scp0000@gmail.com>
2020-04-26 23:34:16 +0900

    05 Counter component

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -12,7 +12,7 @@ export default function counter(state = initialState, action) {
     switch (action.type) {
         case SET_DIFF: return { ...state, diff: action.diff };
         case INCREASE: return { ...state, number: state.number + state.diff };
-        case DECREASE: return { ...state, number: state.numner - state.diff };
+        case DECREASE: return { ...state, number: state.number - state.diff };
         default: return state;
     }
 }
