136ae67629da5f6c9bb84ca5eb76c42b88358f53
bopboy2 <scp0000@gmail.com>
2020-04-27 08:23:27 +0900

    09 using Profiler and React.memo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function TodoItem({ todo, onToggle }) {
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
     return (
         <li
             style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
@@ -9,15 +9,15 @@ function TodoItem({ todo, onToggle }) {
             {todo.text}
         </li>
     );
-}
+})
 
-function TodoList({ todos, onToggle }) {
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
     return (
         <ul>
             {todos.map(todo => (<TodoItem key={todo.id} todo={todo} onToggle={onToggle} />))}
         </ul>
     );
-}
+})
 
 function Todos({ todos, onCreate, onToggle }) {
     const [text, setText] = useState();
@@ -39,4 +39,4 @@ function Todos({ todos, onCreate, onToggle }) {
     );
 }
 
-export default Todos;
+export default React.memo(Todos);
