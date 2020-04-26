48e5f78fcd87767a084804be35b9d2d8169e0872
bopboy2 <scp0000@gmail.com>
2020-04-27 00:36:45 +0900

    07 Todos presentational component

---
+++ src/components/Todos.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function TodoItem({ todo, onToggle }) {
+    return (
+        <li
+            style={{ textDecoration: todo.done ? 'line-through' : 'none' }}
+            onClick={() => onToggle(todo.id)}
+        >
+            {todo.text}
+        </li>
+    );
+}
+
+function TodoList({ todos, onToggle }) {
+    return (
+        <ul>
+            {todos.map(todo => (<TodoItem key={todo.id} todo={todo} onToggle={onToggle} />))}
+        </ul>
+    );
+}
+
+function Todos({ todos, onCreate, onToggle }) {
+    const [text, setText] = useState();
+    const onChange = e => setText(e.target.value);
+
+    return (
+        <div>
+            <form>
+                <input value={text} onChange={onChange} placeholder="할일을 입력하세요" />
+                <button>등록</button>
+            </form>
+            <TodoList todos={todos} onToggle={onToggle} />
+        </div>
+    );
+}
+
+export default Todos;
